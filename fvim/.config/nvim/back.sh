3a5e054a4c925a9ca106d1e16b942f15487f2cf0
hanyu_yan <1824831405@qq.com>
2023-03-23 18:56:28 +0800

    delete files

--- fvim/.config/nvim/back.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mv ~/.config/nvim.LazyVim ~/.config/nvim
-mv ~/.local/share/nvim.LazyVim ~/.local/share/nvim
-mv ~/.local/state/nvim.LazyVim ~/.local/state/nvim
-mv ~/.cache/nvim.LazyVim ~/.cache/nvim
--- fvim/.config/nvim/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mv ~/.config/nvim ~/.config/nvim.LazyVim
-mv ~/.local/share/nvim ~/.local/share/nvim.LazyVim
-mv ~/.local/state/nvim ~/.local/state/nvim.LazyVim
-mv ~/.cache/nvim ~/.cache/nvim.LazyVim
