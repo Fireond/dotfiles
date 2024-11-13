88e37eaef7c4f6e835567bf953a95770a5b97207
Fireond <1824831405@qq.com>
2024-11-13 18:04:05 +0800

    wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,12 +1,41 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 local config = {
 	font_size = 20,
 	font = wezterm.font_with_fallback({
 		{ family = "DejaVuSansMono Nerd Font", weight = "Book" },
+		{ family = "Fira Code", weight = "Book" },
 	}),
+	window_decorations = "RESIZE",
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
 
 	color_scheme = "Catppuccin Mocha",
 }
+
+if wezterm.target_triple == "aarch64-apple-darwin" then
+	-- macos
+elseif wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	-- windows
+	config.keys = {
+		{ key = "w", mods = "CTRL", action = act.CloseCurrentTab({ confirm = false }) },
+		{ key = "t", mods = "CTRL", action = act.SpawnTab("DefaultDomain") },
+	}
+	for i = 1, 8 do
+		table.insert(config.keys, {
+			key = tostring(i),
+			mods = "CTRL",
+			action = act.ActivateTab(i - 1),
+		})
+	end
+	config.mouse_bindings = {
+		{
+			event = { up = { streak = 1, button = "Left" } },
+			mods = "CTRL",
+			action = act.OpenLinkAtMouseCursor,
+		},
+	}
+end
 
 return config
