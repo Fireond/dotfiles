11299672268e740e0350572aeb63aae7017b17a7
Fireond <1824831405@qq.com>
2024-12-14 12:37:24 +0800

    update

--- yazi/.config/yazi/plugins/arrow.yazi/init.lua
+++ yazi/.config/yazi/plugins/arrow.yazi/init.lua
@@ -1,8 +1,8 @@
 --- @sync entry
 return {
-	entry = function(_, args)
+	entry = function(_, job)
 		local current = cx.active.current
-		local new = (current.cursor + args[1]) % #current.files
+		local new = (current.cursor + job.args[1]) % #current.files
 		ya.manager_emit("arrow", { new - current.cursor })
 	end,
 }
