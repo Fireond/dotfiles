6f1f478dd729cef94b63754d36aa9e8d38cc7c27
fireond <yanhy22@mails.tsinghua.edu.cn>
2025-02-24 20:00:00 +0800

    Auto-commit: 2025-02-24 20:00:00

--- nvim/.config/nvim/lua/plugins/dev.lua
+++ nvim/.config/nvim/lua/plugins/dev.lua
@@ -1,6 +1,19 @@
 return {
   {
     dir = "~/Documents/Codes/number-markdown.nvim/",
-    enabled = false,
+    opts = {
+      start_level = 2,
+      auto_update = false,
+      -- function()
+      --   local cur_path = vim.fn.expand("%:p")
+      --   local target_path = vim.fn.expand("~/Documents/Latex")
+      --   if cur_path:sub(1, #target_path) == target_path then
+      --     return true
+      --   else
+      --     return false
+      --   end
+      -- end,
+    },
+    enabled = true,
   },
 }
