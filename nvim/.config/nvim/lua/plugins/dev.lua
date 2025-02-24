ef0c80d43e5dc0c009197f9bdaca9f725c940d93
fireond <yanhy22@mails.tsinghua.edu.cn>
2025-02-24 21:30:00 +0800

    Auto-commit: 2025-02-24 21:30:00

--- nvim/.config/nvim/lua/plugins/dev.lua
+++ nvim/.config/nvim/lua/plugins/dev.lua
@@ -1,18 +1,18 @@
 return {
-  -- {
-  --   dir = "~/Documents/Codes/number-markdown.nvim/",
-  --   ft = "markdown",
-  --   opts = {
-  --     start_level = 2,
-  --     auto_update = function()
-  --       local cur_path = vim.fn.expand("%:p")
-  --       local target_path = vim.fn.expand("~/Documents/Obsidian-Vault")
-  --       if cur_path:sub(1, #target_path) == target_path then
-  --         return true
-  --       else
-  --         return false
-  --       end
-  --     end,
-  --   },
-  -- },
+  {
+    dir = "~/Documents/Codes/number-markdown.nvim/",
+    ft = "markdown",
+    opts = {
+      start_level = 2,
+      auto_update = function()
+        local cur_path = vim.fn.expand("%:p")
+        local target_path = vim.fn.expand("~/Documents/Obsidian-Vault")
+        if cur_path:sub(1, #target_path) == target_path then
+          return false
+        else
+          return true
+        end
+      end,
+    },
+  },
 }
