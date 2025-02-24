1c2a67bc6870d102abbcda10777b8aa9588498d9
fireond <yanhy22@mails.tsinghua.edu.cn>
2025-02-24 20:30:00 +0800

    Auto-commit: 2025-02-24 20:30:00

--- nvim/.config/nvim/lua/plugins/dev.lua
+++ nvim/.config/nvim/lua/plugins/dev.lua
@@ -1,19 +1,18 @@
 return {
-  {
-    dir = "~/Documents/Codes/number-markdown.nvim/",
-    opts = {
-      start_level = 2,
-      auto_update = false,
-      -- function()
-      --   local cur_path = vim.fn.expand("%:p")
-      --   local target_path = vim.fn.expand("~/Documents/Latex")
-      --   if cur_path:sub(1, #target_path) == target_path then
-      --     return true
-      --   else
-      --     return false
-      --   end
-      -- end,
-    },
-    enabled = true,
-  },
+  -- {
+  --   dir = "~/Documents/Codes/number-markdown.nvim/",
+  --   ft = "markdown",
+  --   opts = {
+  --     start_level = 2,
+  --     auto_update = function()
+  --       local cur_path = vim.fn.expand("%:p")
+  --       local target_path = vim.fn.expand("~/Documents/Obsidian-Vault")
+  --       if cur_path:sub(1, #target_path) == target_path then
+  --         return true
+  --       else
+  --         return false
+  --       end
+  --     end,
+  --   },
+  -- },
 }
--- nvim/.config/nvim/lua/plugins/write/markdown.lua
+++ nvim/.config/nvim/lua/plugins/write/markdown.lua
@@ -24,4 +24,20 @@ return {
       vim.g.mkdp_auto_close = 0
     end,
   },
+  {
+    "fireond/number-markdown.nvim",
+    ft = "markdown",
+    opts = {
+      start_level = 2,
+      auto_update = function()
+        local cur_path = vim.fn.expand("%:p")
+        local target_path = vim.fn.expand("~/Documents/Obsidian-Vault")
+        if cur_path:sub(1, #target_path) == target_path then
+          return true
+        else
+          return false
+        end
+      end,
+    },
+  },
 }
