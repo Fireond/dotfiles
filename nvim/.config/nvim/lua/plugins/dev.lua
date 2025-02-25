4b822f94e797245168b83c6163d2a6b602402100
fireond <yanhy22@mails.tsinghua.edu.cn>
2025-02-25 17:00:00 +0800

    Auto-commit: 2025-02-25 17:00:00

--- nvim/.config/nvim/lua/plugins/dev.lua
+++ nvim/.config/nvim/lua/plugins/dev.lua
@@ -1,30 +1,30 @@
 return {
-  -- {
-  --   dir = "~/Documents/Codes/number-markdown.nvim/",
-  --   ft = "markdown",
-  --   opts = {
-  --     start_level = 2,
-  --     auto_update = function()
-  --       local cur_path = vim.fn.expand("%:p")
-  --       local target_path = vim.fn.expand("~/Documents/Obsidian-Vault")
-  --       local math_path = vim.fn.expand("~/Documents/Obsidian-Vault/02-math")
-  --       if cur_path:sub(1, #math_path) == math_path then
-  --         return false
-  --       elseif cur_path:sub(1, #target_path) == target_path then
-  --         return true
-  --       else
-  --         return false
-  --       end
-  --     end,
-  --   },
-  --   keys = {
-  --     {
-  --       "<leader>uH",
-  --       function()
-  --         require("number-markdown").toggle_auto_update()
-  --       end,
-  --       desc = "Toggle auto number headers",
-  --     },
-  --   },
-  -- },
+  {
+    dir = "~/Documents/Codes/number-markdown.nvim/",
+    ft = "markdown",
+    opts = {
+      start_level = 2,
+      auto_update = function()
+        local cur_path = vim.fn.expand("%:p")
+        local target_path = vim.fn.expand("~/Documents/Obsidian-Vault")
+        local math_path = vim.fn.expand("~/Documents/Obsidian-Vault/02-math")
+        if cur_path:sub(1, #math_path) == math_path then
+          return false
+        elseif cur_path:sub(1, #target_path) == target_path then
+          return true
+        else
+          return false
+        end
+      end,
+    },
+    keys = {
+      {
+        "<leader>uH",
+        function()
+          require("number-markdown").toggle_auto_update()
+        end,
+        desc = "Toggle auto number headers",
+      },
+    },
+  },
 }
