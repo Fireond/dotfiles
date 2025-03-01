86d31b567869015f120ca620f8677b2ca875c909
fireond <yanhy22@mails.tsinghua.edu.cn>
2025-03-01 21:00:01 +0800

    Auto-commit: 2025-03-01 21:00:01

--- nvim/.config/nvim/lua/plugins/write/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/write/obsidian.lua
@@ -23,6 +23,17 @@ return {
       -- note_id_func = function(title)
       --   return title
       -- end,
+      note_id_func = function(title)
+        local suffix = ""
+        if title ~= nil then
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        return tostring(os.time()) .. "-" .. suffix
+      end,
       wiki_link_func = "prepend_note_path",
       templates = {
         folder = "templates",
