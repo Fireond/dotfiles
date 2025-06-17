749ab1e8c54c62ddf5456d08841ded515e2cb8ee
fireond <yanhy22@mails.tsinghua.edu.cn>
2025-06-17 21:30:01 +0800

    Auto-commit: 2025-06-17 21:30:01

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,6 +1,6 @@
-require("full-border"):setup()
+-- require("full-border"):setup()
 require("starship"):setup()
--- https://yazi-rs.github.io/docs/configuration/yazi
+-- -- https://yazi-rs.github.io/docs/configuration/yazi
 function Linemode:size_and_mtime()
 	local time = math.floor(self._file.cha.mtime or 0)
 	if time == 0 then
