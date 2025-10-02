65ee5e26a6fcba40933a0eecef1db8349e31029d
fireond <yanhy22@mails.tsinghua.edu.cn>
2025-10-02 11:30:00 +0800

    Auto-commit: 2025-10-02 11:30:00

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     { import = "plugins.write" },
     { import = "plugins" },
   },
+  git = { url_format = "git@github.com:%s.git" },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
