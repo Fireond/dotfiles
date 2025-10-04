220de4c4876b71a65f7b62ab6f580903ef960dbf
fireond <yanhy22@mails.tsinghua.edu.cn>
2025-10-04 12:00:00 +0800

    Auto-commit: 2025-10-04 12:00:00

--- nvim/.config/nvim/lua/plugins/image.lua
+++ nvim/.config/nvim/lua/plugins/image.lua
@@ -95,7 +95,7 @@ return {
           notify = true, -- show a notification on error
         },
         math = {
-          enabled = true,
+          enabled = false,
           latex = {
             font_size = "normalsize", -- see https://www.sascha-frank.com/latex-font-size.html
             packages = {
