9ebe7ce5528b8b1efc42f5f5008679b2f83c84e0
fireond <yanhy22@mails.tsinghua.edu.cn>
2025-10-30 18:30:00 +0800

    Auto-commit: 2025-10-30 18:30:00

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -5,13 +5,30 @@ config.load_autoconfig()
 c.auto_save.session = True
 catppuccin.setup(c, "macchiato", True)
 
-# c.fonts.default_family = "WenQuanYi Micro Hei"
-# c.fonts.default_size = "14pt"
 c.fonts.web.family.standard = "WenQuanYi Micro Hei"
 c.fonts.web.family.serif = "WenQuanYi Micro Hei"
 c.fonts.web.size.default = 14
 c.fonts.web.size.default_fixed = 14
 
+
+c.tabs.position = "left"
+c.tabs.width = 150
+
+c.url.searchengines = {
+    "DEFAULT": "https://www.google.com/search?q={}",
+    "g": "https://www.google.com/search?q={}",
+    "wk": "https://en.wikipedia.org/w/index.php?search={}",
+    "gt": "https://github.com/search?q={}",
+    "zh": "https://www.zhihu.com/search?q={}&type=content",
+    "bl": "https://search.bilibili.com/all?keyword={}",
+}
+
+
 # keybinds
 config.bind(",c", "open -t https://chatgpt.com")
 config.bind(",b", "open -t https://bilibili.com")
+config.bind(",z", "open -t https://zhihu.com")
+config.bind(",i", "open -t https://info.tsinghua.edu.cn")
+config.bind(",l", "open -t https://login.tsinghua.edu.cn")
+
+config.bind("<Ctrl-Alt-c>", "config-cycle tabs.show always never")
