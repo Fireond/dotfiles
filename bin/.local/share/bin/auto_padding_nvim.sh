6666efc63958c6e63951c3cc645d1ac26672acc9
fireond <yanhy22@mails.tsinghua.edu.cn>
2025-12-20 13:30:00 +0800

    Auto-commit: 2025-12-20 13:30:00

--- bin/.local/share/bin/auto_padding_nvim.sh
+++ bin/.local/share/bin/auto_padding_nvim.sh
@@ -6,11 +6,11 @@ KITTY_SOCKET=$(echo $KITTY_LISTEN_ON)
 # We can control the current kitty terminal using socket
 # and using "kitten" : https://sw.kovidgoyal.net/kitty/remote-control/#remote-control-via-a-socket
 if [ -n "$KITTY_SOCKET" ]; then
-  kitten @ --to $KITTY_SOCKET set-spacing padding=0
+  kitten @ --to "$KITTY_SOCKET" set-spacing padding=0
 fi
 
-nvim $*
+nvim "$@"
 
 if [ -n "$KITTY_SOCKET" ]; then
-  kitten @ --to $KITTY_SOCKET set-spacing padding=default
+  kitten @ --to "$KITTY_SOCKET" set-spacing padding=default
 fi
