7f899be3c72b34c8f0248870ac076ebe8f88b113
fireond <yanhy22@mails.tsinghua.edu.cn>
2025-12-26 22:30:00 +0800

    Auto-commit: 2025-12-26 22:30:00

--- nvim/.config/nvim/lua/plugins/write/markdown.lua
+++ nvim/.config/nvim/lua/plugins/write/markdown.lua
@@ -38,7 +38,7 @@ return {
     build = "cd app && yarn install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
-      vim.g.mkdp_auto_start = 1
+      vim.g.mkdp_auto_start = 0
       vim.g.mkdp_auto_close = 0
       vim.g.mkdp_combine_preview = 1
       vim.g.mkdp_browser = "firefox"
