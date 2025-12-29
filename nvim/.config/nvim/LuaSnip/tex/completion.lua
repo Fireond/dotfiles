5b02c0e3cd31ba3088f50d83b0ceb6d3c531d16a
fireond <yanhy22@mails.tsinghua.edu.cn>
2025-12-29 10:30:00 +0800

    Auto-commit: 2025-12-29 10:30:00

--- nvim/.config/nvim/LuaSnip/tex/completion.lua
+++ nvim/.config/nvim/LuaSnip/tex/completion.lua
@@ -181,8 +181,8 @@ return {
   s(
     { trig = "sum", snippetType = "autosnippet" },
     c(1, {
-      sn(nil, { t("\\sum_{"), i(1, "i=1"), t("} ") }),
-      -- sn(nil, { t("\\sum_{"), i(1, "n=1"), t("}^{"), i(2, "\\infty"), t("} ") }),
+      -- sn(nil, { t("\\sum_{"), i(1, "i=1"), t("} ") }),
+      sn(nil, { t("\\sum_{"), i(1, "n=1"), t("}^{"), i(2, "\\infty"), t("} ") }),
     }),
     { condition = tex.in_mathzone }
   ),
