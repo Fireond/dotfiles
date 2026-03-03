1d73d480050b04277ea5021d488f9bea5c677418
fireond <yanhy22@mails.tsinghua.edu.cn>
2026-03-03 18:30:00 +0800

    Auto-commit: 2026-03-03 18:30:00

--- nvim/.config/nvim/lua/plugins/default/default.lua
+++ nvim/.config/nvim/lua/plugins/default/default.lua
@@ -1,10 +1,6 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- keys[#keys + 1] = { "K", "a<cr><Esc>k$" }
-    end,
     opts = {
       setup = {
         clangd = function(_, opts)
