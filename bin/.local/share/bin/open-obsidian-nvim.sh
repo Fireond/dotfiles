5597d43992ad4d71d842ba10475f20845afe8c47
fireond <yanhy22@mails.tsinghua.edu.cn>
2026-03-04 21:26:25 +0800

    Auto-commit: 2026-03-04 21:26:25

--- bin/.local/share/bin/open-obsidian-nvim.sh
+++ bin/.local/share/bin/open-obsidian-nvim.sh
@@ -1,2 +1,4 @@
-#!/bin/sh
-exec kitty -e /home/fireond/.local/share/bin/auto_padding_nvim.sh -S "/home/fireond/.local/state/nvim/sessions/\%home\%fireond\%Documents\%sync-server\%Obsidian-Vault.vim"
+#!/bin/zsh
+session_name="${HOME//\//\\%}\%Documents\%sync-server\%Obsidian-Vault.vim"
+exec kitty -e "$HOME/.local/share/bin/auto_padding_nvim.sh" -S "$HOME/.local/state/nvim/sessions/$session_name"
+# exec kitty -e /home/fireond/.local/share/bin/auto_padding_nvim.sh -S "/home/fireond/.local/state/nvim/sessions/\%home\%fireond\%Documents\%sync-server\%Obsidian-Vault.vim"
