9be25c281bb775b5e5b8af8317f316b2522923de
fireond <yanhy22@mails.tsinghua.edu.cn>
2026-03-30 15:00:00 +0800

    Auto-commit: 2026-03-30 15:00:00

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,6 @@
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.wo.foldmethod = "expr"
+vim.opt.expandtab = false
+vim.opt.softtabstop = 0
 
 vim.treesitter.language.register("latex", "tikz")
